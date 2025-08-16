7c94ee450f546918da4432ece02dba6720edb8b6
Pranav solanki <github39645@gmail.com>
2025-08-16 19:31:33 +0530

    new changes

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectToMongoose = async () => {
-    await mongoose.connect("mongodb://localhost:27017/");
-    console.log("Connected to MongoDB");
+  await mongoose.connect("mongodb://localhost:27017/");
+  console.log("Connected to MongoDB");
 };
 
 module.exports = connectToMongoose;
--- index.js
+++ index.js
@@ -5,11 +5,14 @@ connectToMongoose();
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
 
