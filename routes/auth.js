3e71aece746b47b35a166fbef5a98c9a66307ee3
Pranav solanki <github39645@gmail.com>
2025-08-17 12:13:31 +0530

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
-const bcrypt=require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const jwt_key = "shhh";
 router.post(
   "/createuser",
   [
@@ -21,7 +22,7 @@ router.post(
       return res.status(400).json({ errors: result.array() });
     }
     const salt = bcrypt.genSaltSync(10);
-    const secpass=await bcrypt.hash(req.body.password,salt)
+    const secpass = await bcrypt.hash(req.body.password, salt);
     try {
       // Create user
       const user = await User.create({
@@ -29,14 +30,48 @@ router.post(
         email: req.body.email,
         password: secpass, // ✅ fixed typo here
       });
-      const jwt_key="shhh"
-      const data={
-        user:{
-          id:user.id
-        }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, jwt_key);
+      res.status(201).json({ authtoken }); // Success response
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Server error" });
+    }
+  }
+);
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cant be blank").exists(),
+  ],
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Enter valid credentials:" });
       }
-      const authtoken=jwt.sign(data,jwt_key);
-      res.status(201).json({authtoken}); // Success response
+      const passcomp = await bcrypt.compare(password, user.password);
+      if (!passcomp) {
+        return res.status(400).json({ error: "Enter valid credentials:" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, jwt_key);
+      res.status(201).json({ authtoken }); // Success response
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Server error" });
